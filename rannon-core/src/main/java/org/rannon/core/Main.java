d548bdc4a167e0f42285197421501f9580567977
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-05-27 16:57:15 +0900

    add input data

--- rannon-core/src/main/java/org/rannon/core/Main.java
+++ rannon-core/src/main/java/org/rannon/core/Main.java
@@ -1,6 +1,7 @@
 package org.rannon.core;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -9,8 +10,10 @@ public class Main {
         AnnotationService service = new AnnotationService();
 
         List<Dialogue> list = new ArrayList<>();
-        Dialogue dialogue = new Dialogue("天気");
-        list.add(dialogue);
+        Dialogue dialogue_1 = new Dialogue("天気がいい");
+        Dialogue dialogue_2 = new Dialogue("今日の天気は晴れ");
+        Dialogue dialogue_3 = new Dialogue("明日は雨");
+        list.addAll(Arrays.asList(dialogue_1, dialogue_2, dialogue_3));
 
         // execute
         List<AnnotatedDialogue> result = service.annotate(list);
