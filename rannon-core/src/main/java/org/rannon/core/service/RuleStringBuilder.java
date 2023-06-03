15dfc776dcb16d562aba213399dfc9178188acca
Hami1ton <53397268+Hami1ton@users.noreply.github.com>
2023-06-03 10:36:22 +0900

    add test

--- rannon-core/src/main/java/org/rannon/core/service/RuleStringBuilder.java
+++ rannon-core/src/main/java/org/rannon/core/service/RuleStringBuilder.java
@@ -7,14 +7,23 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class RuleStringBuilder {
 
-    static String buildRuleString() {
+    static String buildRuleString(List<TextMatchTagRule> textMatchTagRules) throws URISyntaxException, IOException {
         StringBuilder sb = new StringBuilder("");
-        sb.append("");
+        sb.append(RuleStringBuilder.buildImportSection());
+        sb.append("\n");
 
-        return "";
+        int counta = 0;
+        for(TextMatchTagRule textMatchTagRule : textMatchTagRules) {
+            sb.append(RuleStringBuilder.buildRuleSection(textMatchTagRule, counta));
+            sb.append("\n");
+            counta += 1;
+        }
+
+        return sb.toString();
     }
 
     static String buildImportSection() throws URISyntaxException, IOException {
@@ -23,10 +32,11 @@ public class RuleStringBuilder {
         return text;
     }
 
-    static String buildRuleSection(TextMatchTagRule textMatchTagRule) throws URISyntaxException, IOException {
+    static String buildRuleSection(TextMatchTagRule textMatchTagRule, int ruleIndex) throws URISyntaxException, IOException {
         Path file = Paths.get(RuleStringBuilder.class.getClassLoader().getResource("RuleSectionTemplate.drl").toURI());
         String text = Files.readString(file);
 
+        text = text.replace("TEMPLATE_INDEX", String.valueOf(ruleIndex));
         text = text.replace("TEMPLATE_MATCH_TEXT", textMatchTagRule.matchText());
         text = text.replace("TEMPLATE_TAG", textMatchTagRule.tag());
         return text;
--- rannon-core/src/test/java/org/rannon/core/service/RuleStringBuilderTest.java
+++ rannon-core/src/test/java/org/rannon/core/service/RuleStringBuilderTest.java
@@ -8,17 +8,27 @@ import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RuleStringBuilderTest {
 
+
     @Test
-    void test() {
+    void test_buildRuleString() throws URISyntaxException, IOException {
 
-        String ruleString = RuleStringBuilder.buildRuleString();
+        List<TextMatchTagRule> textMatchTagRules = new ArrayList<>();
+        textMatchTagRules.add(new TextMatchTagRule("天気", "天気予報タグ"));
+        textMatchTagRules.add(new TextMatchTagRule("お菓子", "スイーツタグ"));
 
-        assertEquals("", ruleString);
+        String ruleString = RuleStringBuilder.buildRuleString(textMatchTagRules);
+
+        Path file = Paths.get(RuleStringBuilder.class.getClassLoader().getResource("BuildRuleTemplateTest.drl").toURI());
+        String text = Files.readString(file);
+
+        assertEquals(text, ruleString);
     }
 
     @Test
@@ -35,7 +45,7 @@ public class RuleStringBuilderTest {
     @Test
     void test_RuleSectionTemplate() throws URISyntaxException, IOException {
 
-        String ruleString = RuleStringBuilder.buildRuleSection(new TextMatchTagRule("天気", "天気予報タグ"));
+        String ruleString = RuleStringBuilder.buildRuleSection(new TextMatchTagRule("天気", "天気予報タグ"), 0);
 
         Path file = Paths.get(RuleStringBuilder.class.getClassLoader().getResource("RuleSectionTemplateTest.drl").toURI());
         String text = Files.readString(file);
